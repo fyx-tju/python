bba0854fdcaa506cc07419599c0b7818437a80c5
fyx <fuyuxin_tju@163.com>
2018-09-20 22:53:17 +0800

    commmit auto

--- pyplot.py
+++ pyplot.py
@@ -1,12 +1,19 @@
 import numpy as np
-import matplotlib.pylab as plt
+import matplotlib
+#matplotlib.use('Qt5Agg')
+import matplotlib.pyplot as plt
 import math
+import pandas as pd
 
 Xp1=[]
 Yp1=[]
 tag = False
 file= "1.txt" if tag else "2.txt"
 fun = lambda x:abs(x-1)
+#series = pd.Series([1,2,3,4],index=["a","b","c","d"])
+#plt.plot(series)
+#plt.title("an example to plot series")
+#plt.show()
 
 print(sorted([1,3,5,-1],key=fun))
 
