40398af101551094ae3978c0e59faeba11d43a13
fyx <fuyuxin_tju@163.com>
2018-11-28 23:56:45 +0800

    commmit auto

--- tarfile.py
+++
@@ -1,9 +0,0 @@
-#tar文件解压
-import os
-import tarfile
-
-file = os.path.join("/home/fyx","a")
-statinfo = os.stat(file)
-print statinfo.st_size
-if not os.path.exists("/home/fyx/a"):
-  tarfile.open("a.tar.gz",'r:gz').extractall("/home/fyx/")
